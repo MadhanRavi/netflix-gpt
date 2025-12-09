a06f3e86046dbab6675ffb632dcf65b070571451
Madhan R <madhanravi@gmail.com>
2025-12-09 19:42:39 +0530

    CRA & Tailwind CSS

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-center m-4">
+      <h1 className="font-bold text-2xl text-red-600">Netflix GPT</h1>
+    </div>
+  );
+}
+
+export default App;
